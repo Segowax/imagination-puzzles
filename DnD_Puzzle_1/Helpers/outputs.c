9fefecffa92b8417a9eb2496f94d79555baec973
Segowax <kosmicznybandyta@gmail.com>
2022-11-05 23:28:30 +0100

    Final form of my first DnD puzzle!

---
+++ DnD_Puzzle_1/Helpers/outputs.c
@@ -0,0 +1,17 @@
+/*
+ * leds.c
+ *
+ *  Created on: 1 lis 2022
+ *      Author: KosmicznyBandyta
+ */
+
+#include <avr/io.h>
+
+#include "../Headers/outputs.h"
+
+void init_leds() {
+	OUTPUTS_DDR |= OUTPUT1_PIN | OUTPUT2_PIN | OUTPUT3_PIN | OUTPUT4_PIN | OUTPUT5_PIN | OUTPUT6_PIN
+			| OUTPUT7_PIN | OUTPUT8_PIN;
+	OUTPUTS_PORT |= OUTPUT1_PIN | OUTPUT2_PIN | OUTPUT3_PIN | OUTPUT4_PIN | OUTPUT5_PIN | OUTPUT6_PIN
+			| OUTPUT7_PIN | OUTPUT8_PIN;
+}
--- DnD_Puzzle_1/main.c
+++ DnD_Puzzle_1/main.c
@@ -10,11 +10,12 @@
 #include <avr/interrupt.h>
 
 #include "Headers/keys.h"
-#include "Headers/leds.h"
 #include "Headers/led_display.h"
+#include "Headers/outputs.h"
 
 int main(void) {
 	init_buttons();
+	init_leds();
 	init_led_display();
 
 	// enable global interrupts - secret function
@@ -23,7 +24,14 @@ int main(void) {
 	uint16_t number = count_to_zero_in(300);
 
 	while (1) {
-		if (number != 0)
+		if (number != 0 || !IS_KEY1_DOWN)
 			number = count_to_zero_continue(number);
+		if (IS_KEY2_DOWN) {
+			ON_OUTPUT1_PIN;
+			_delay_ms(1000);
+			OFF_OUTPUT1_PIN;
+			dispose_led_display();
+			break;
+		}
 	}
 }
