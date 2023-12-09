64fe21627596b8e1064e8c2e6a5a069d6eb754fe
Segowax <kosmicznybandyta@gmail.com>
2023-12-09 12:11:50 +0100

    display_text function added

--- DnD_Puzzle_3/main.c
+++ DnD_Puzzle_3/main.c
@@ -7,14 +7,28 @@
 
 #include "lcd_hd44780/lcd.h"
 
+//private zone
+void display_text(char text[], uint16_t ms);
+
 int main() {
 
 	init_lcd();
-	char tab[] = "Test" "\x7f" "Nas";
-	lcd_str(tab);
 
 	_delay_ms(1000);
 
 	while (1) {
+		display_text("Test", 1000);
+		display_text("Test1", 1000);
+		display_text("Test2", 1000);
+		display_text("Test3", 1000);
+	}
+}
+
+void display_text(char text[], uint16_t ms) {
+	lcd_cls();
+	lcd_str(text);
+	while (ms) {
+		_delay_ms(1);
+		ms--;
 	}
 }
